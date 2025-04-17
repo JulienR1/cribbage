387e7db635c3d182720843e88ef3e80f92a6488f
julienrousseau <julrousseau20@gmail.com>
2025-04-17 11:27:48 -0400

    reworked deck structure

--- internal/deck/deck.go
+++ internal/deck/deck.go
@@ -1,29 +1,44 @@
 package deck
 
 import (
+	"errors"
 	"math/rand/v2"
 
 	"github.com/julienr1/cribbage/internal/assert"
 )
 
-type Deck [52]Card
+var EmptyDeckErr = errors.New("deck is empty")
 
-func New() Deck {
+type Deck struct {
+	drawIndex int
+	cards     [52]Card
+}
+
+func New() *Deck {
 	var d Deck
 	for i, color := range []Color{SPADES, CLUBS, DIAMONDS, HEARTS} {
 		for value := range 13 {
 			c, err := NewCard(uint8(value)+1, color)
 			assert.AssertE(err)
-			d[i*13+value] = c
+			d.cards[i*13+value] = c
 		}
 	}
-	return d
+	return &d
 }
 
-func (d Deck) Shuffle() Deck {
-	for i := range len(d) {
-		target := i + rand.IntN(len(d)-i)
-		d[target], d[i] = d[i], d[target]
+func (d *Deck) Shuffle() {
+	for i := range len(d.cards) {
+		target := i + rand.IntN(len(d.cards)-i)
+		d.cards[target], d.cards[i] = d.cards[i], d.cards[target]
 	}
-	return d
+}
+
+func (d *Deck) Draw() (Card, error) {
+	if d.drawIndex >= len(d.cards) {
+		return 0, EmptyDeckErr
+	}
+
+	c := d.cards[d.drawIndex]
+	d.drawIndex++
+	return c, nil
 }
--- internal/deck/deck_test.go
+++ internal/deck/deck_test.go
@@ -7,15 +7,48 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestDraw(t *testing.T) {
+	d := deck.New()
+	c, err := d.Draw()
+
+	assert.NoError(t, err)
+	expected, _ := deck.NewCard(1, deck.SPADES)
+	assert.Equal(t, expected, c)
+}
+
+func TestEmptyDeck(t *testing.T) {
+	d := deck.New()
+	var previous deck.Card = 0
+
+	for range 52 {
+		current, err := d.Draw()
+		assert.NoError(t, err)
+		assert.NotEqual(t, previous, current)
+		previous = current
+	}
+
+	_, err := d.Draw()
+	assert.Error(t, err)
+}
+
 func TestShuffle(t *testing.T) {
 	decksAreIdentical := true
 
 	for range 3 {
-		d1 := deck.New().Shuffle()
-		d2 := deck.New().Shuffle()
+		d1 := deck.New()
+		d2 := deck.New()
 
-		for i := range len(d1) {
-			if d1[i] != d2[i] {
+		d1.Shuffle()
+		d2.Shuffle()
+
+		for range 52 {
+			c1, e1 := d1.Draw()
+			c2, e2 := d2.Draw()
+
+			assert.NoError(t, e1)
+			assert.NoError(t, e2)
+
+			if c1 != c2 {
 				decksAreIdentical = false
 			}
 		}
