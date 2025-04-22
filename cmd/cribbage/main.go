8517cb9d43ff2167c51eab8be8a83c1c677008cb
julienrousseau <julrousseau20@gmail.com>
2025-04-22 16:13:30 -0400

    test main func

--- cmd/cribbage/main.go
+++ cmd/cribbage/main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
+	"github.com/julienr1/cribbage/internal/assert"
 	"github.com/julienr1/cribbage/internal/game"
 )
 
 func main() {
 	fmt.Println("cribbage")
 
-	players := []*game.Hand{
-		game.NewHand(),
-		game.NewHand(),
+	var playerCount int = 2
+	if len(os.Args) > 1 {
+		count, err := strconv.Atoi(os.Args[1])
+		assert.AssertE(err)
+		playerCount = count
 	}
+
+	players := make(game.Hands, playerCount)
+	for i := range playerCount {
+		players[i] = game.NewHand()
+	}
+
 	game := game.New(players)
 
-	fmt.Println(game)
+	log.Println("--- HANDS ---")
+	game.Next()
+	log.Println("\n", players.String())
+
+	log.Println("--- CRIB ---")
+	game.Next()
+
 }
